3bac351c78cf97a6029be7260415012f22aeac1e
Jeff Casimir <jeff@casimircreative.com>
2012-03-12 14:49:21 -0400

    Initial Commit

---
+++ eventmanager.rb
@@ -0,0 +1,43 @@
+$LOAD_PATH << './'
+require 'csv'
+require 'attendee'
+
+class EventManager
+  CSV_OPTIONS = {:headers => true, :header_converters => :symbol}
+
+  attr_accessor :attendees
+
+  def initialize(filename, options = CSV_OPTIONS)
+    load_attendees(CSV.open(filename, options))
+  end
+
+  def print_names
+    attendees.each do |attendee|
+      puts attendee.full_name
+    end
+  end
+
+  def print_zipcodes
+    attendees.each do |attendee|
+      puts attendee.zipcode
+    end
+  end
+
+  def print_phone_numbers
+    attendees.each do |attendee|
+      puts attendee.phone_number
+    end
+  end
+
+  private
+
+  def load_attendees(file)
+    self.attendees = file.collect { |line| Attendee.new(line) }
+  end
+
+end
+
+em = EventManager.new("event_attendees.csv")
+#em.print_zipcodes
+em.print_phone_numbers
+#em.print_names
